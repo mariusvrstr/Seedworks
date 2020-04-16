e9d36c225c6eb8f5d4b9dce6d2803269c2b3858a
marius <marius.vrstr@gmail.com>
2020-04-16 17:47:58 +0200

    Added mocking for repositories

---
+++ DAL/Spike.App.Tests/WebsiteRespositoryMockTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Spike.App.Repositories.Entities;
+using Spike.App.Repositories.Repositories;
+using Spike.App.Tests.Builders;
+using Spike.Seedworks.Conmmon.DAL;
+
+namespace Spike.App.Tests
+{
+    [TestClass]
+    public class WebsiteRespositoryMockTests {
+
+        private IRepository<Website> basicWebsiteRepo;
+        private const bool USE_MOCKED_SERVICE = true;
+
+        public WebsiteRespositoryMockTests()
+        {
+            if (USE_MOCKED_SERVICE)
+            {
+                // Mocked Repository
+                basicWebsiteRepo = MockRepository.GenerateMock<IRepository<Website>>();
+
+                basicWebsiteRepo.Stub(e => e.Add(Arg<Website>.Is.Anything))
+                    .WhenCalled(x =>
+                    {
+                        // Return what was passed in
+                        x.ReturnValue = (Website)x.Arguments[0];
+                    });
+
+                // Always return a Google Website when searching by ID
+                basicWebsiteRepo.Stub(e =>
+                    e.GetById(Arg<Guid>.Is.Anything)).Return(new WebsiteBuilder().GoogleWebsite().Build());
+            }
+            else
+            {
+                basicWebsiteRepo = new BasicWebsiteRepository();
+            }
+        }
+
+        [TestMethod]
+        public void TestAddWebsiteToBasicBase()
+        {
+            Website response;
+            var facebookWebsite = new WebsiteBuilder().FacebookWebsite().Build();
+
+            try
+            {
+                response = basicWebsiteRepo.Add(facebookWebsite);
+                basicWebsiteRepo.Save();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Assert.Fail();
+                throw;
+            }
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(facebookWebsite.CompanyName, response.CompanyName);
+            Assert.AreEqual(facebookWebsite.WebsiteUrl, response.WebsiteUrl);
+        }
+    }
+}
